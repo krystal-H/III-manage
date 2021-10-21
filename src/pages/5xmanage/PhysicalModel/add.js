7ea43560462d050df418555af23557728bbe8d9e
ly <yang.li@clife.cn>
2021-10-21 17:41:49 +0800

    no message

--- src/pages/5xmanage/PhysicalModel/add.js
+++ src/pages/5xmanage/PhysicalModel/add.js
@@ -1,29 +1,85 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Input, Button, Select, Icon, Radio, Modal, Form, Tooltip, DatePicker, Upload } from 'antd';
-import TitleTab from '../../../components/TitleTab';
-import TableCom from '../../../components/Table';
-import { upFile } from '../../../apis/physical'
+import { Table, Input, Button, Select, Icon, Radio, Modal, Form, Tabs, DatePicker, Upload, message } from 'antd';
+import TableCom from './TableCom';
+import { upFile,newData } from '../../../apis/physical'
 import './index.less'
 
 const FormItem = Form.Item
 const { TextArea } = Input;
+const { TabPane } = Tabs;
+//处理数据
+function delaData(data) {
+    let newData = []
+    data.forEach(item => {
+        if (!item.funcParamList || !item.funcParamList.length) return
+        item.funcParamList.forEach(item2 => {
+            let newItem = JSON.parse(JSON.stringify(item))
+            newData.push({ ...newItem, ...item2 })
+        })
+    })
+    newData.forEach((item, index) => {
+        item.key = index
+    })
+    return newData
+}
 function Addmodal({ form, addVis, handleCancel, handleOk, optionList }) {
     const { getFieldDecorator, validateFields } = form;
+    const [currentTab, setCurrentTab] = useState('1')
+    const [tableData, setTableData] = useState([])
+    const [showData, setShowData] = useState([])
     const sundata = () => {
-        validateFields().then(val=>{
-
+        validateFields().then(val => {
+            console.log(val,'提交的wenj')
+            newData(val).then(res=>{
+                if(res.data.code == 0){
+                    message.success('新增成功');
+                }
+            })
         })
     }
     //导入
     const customRequest = (option) => {
         upFile({ file: option.file }).then(res => {
+            if (res.data.code == 0) {
+                let data = delaData(res.data.data.standard || [])
+                setTableData(data)
+
+                tabcallback('1', data)
+            }
 
         })
     }
     const formItemLayout = {
-        labelCol: { span: 6 },
-        wrapperCol: { span: 14 },
+        labelCol: { span: 2 },
+        wrapperCol: { span: 22 },
     };
+
+
+    const tabcallback = (val, data) => {
+        data = data || tableData
+        setCurrentTab(val)
+        let funcType = ['properties', 'events', 'services']
+        let arr = data.filter(item => {
+            if (item.funcType == funcType[val - 1]) {
+                return item
+            }
+        })
+        setShowData(arr)
+    }
+    const beforeUpload = (file) => {
+        return true
+    }
+    const normFile = e => {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e && e.fileList;
+    };
+    const onRemove =file=>{
+        setTableData([])
+        setShowData([])
+    }
     return (
         <div>
             <Modal
@@ -31,7 +87,7 @@ function Addmodal({ form, addVis, handleCancel, handleOk, optionList }) {
                 visible={addVis}
                 onOk={sundata}
                 onCancel={handleCancel}
-                width={'700px'}
+                width={'1200px'}
             >
                 <div>
                     <Form {...formItemLayout}>
@@ -62,8 +118,8 @@ function Addmodal({ form, addVis, handleCancel, handleOk, optionList }) {
                             )}
                         </Form.Item>
                         <FormItem label="模板设置">
-                            {getFieldDecorator('file', { rules: [{ required: true}], valuePropName: 'fileList', })(
-                                <Upload customRequest={customRequest} showUploadList={false}>
+                            {getFieldDecorator('file', { rules: [{ required: true }], valuePropName: 'fileList', getValueFromEvent: normFile, })(
+                                <Upload customRequest={customRequest} beforeUpload={beforeUpload} listType="picture" onRemove ={onRemove }>
                                     <Icon type="upload" /> 选择模板
                                 </Upload>
                             )}
@@ -72,6 +128,21 @@ function Addmodal({ form, addVis, handleCancel, handleOk, optionList }) {
                             {getFieldDecorator('remark', { rules: [{ required: true }] })(
                                 <TextArea style={{ width: '100%' }} ></TextArea>
                             )}
+                        </FormItem>
+                        <FormItem label="物模型详情">
+                            <div>
+                                <Tabs activeKey={currentTab} onChange={tabcallback}>
+                                    <TabPane tab="属性" key="1">
+                                    </TabPane>
+                                    <TabPane tab="事件" key="2">
+                                    </TabPane>
+                                    <TabPane tab="服务" key="3">
+                                    </TabPane>
+                                </Tabs>
+                                <div>
+                                    <TableCom dataSource={showData} pagination={false} />
+                                </div>
+                            </div>
                         </FormItem>
                     </Form>
                 </div>
